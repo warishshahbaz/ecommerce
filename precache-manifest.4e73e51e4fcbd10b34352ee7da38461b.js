844c808e9668cdfdacd30f9a603b430f84b2b3e3
md shahbaz warish <mdshahbazwarish1996@gmail.com>
2022-08-21 23:50:48 +0530

    Updates

---
+++ precache-manifest.4e73e51e4fcbd10b34352ee7da38461b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc504782ad45495d8258",
+    "url": "/ecommerce.github.io/static/css/main.85ae2791.chunk.css"
+  },
+  {
+    "revision": "dc504782ad45495d8258",
+    "url": "/ecommerce.github.io/static/js/main.dc504782.chunk.js"
+  },
+  {
+    "revision": "b0a75d9a68296b9b3ae9",
+    "url": "/ecommerce.github.io/static/js/1.b0a75d9a.chunk.js"
+  },
+  {
+    "revision": "8d7736e2bf420afe235c",
+    "url": "/ecommerce.github.io/static/css/2.c5911646.chunk.css"
+  },
+  {
+    "revision": "8d7736e2bf420afe235c",
+    "url": "/ecommerce.github.io/static/js/2.8d7736e2.chunk.js"
+  },
+  {
+    "revision": "0f04448c1c903160fe36",
+    "url": "/ecommerce.github.io/static/js/runtime~main.0f04448c.js"
+  },
+  {
+    "revision": "f238c0c5bcd883a676aed37d8f04af47",
+    "url": "/ecommerce.github.io/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/ecommerce.github.io/precache-manifest.4e73e51e4fcbd10b34352ee7da38461b.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/ecommerce.github.io/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
