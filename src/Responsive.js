303e1c0440d837fd38abe587ab18293fb5a53474
md shahbaz warish <mdshahbazwarish1996@gmail.com>
2022-08-22 12:49:23 +0530

    Updates

--- src/Responsive.js
+++
@@ -1,9 +0,0 @@
-import {css} from 'styled-components'
-
-export const mobile = (Props) => {
-    return css`
-        @media only screen and (max-width:400px) {
-            ${Props}
-        }
-    `
-}
